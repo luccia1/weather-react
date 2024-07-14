91032949e19fd83bb1aeae8af669a9a663421740
lucy <139963171+luccia1@users.noreply.github.com>
2024-07-14 09:21:23 -0700

    added css style on my weather.jsx and app.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,13 +4,22 @@ import viteLogo from '/vite.svg'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import './App.css'
+import Weather from './Weather';
 
 function App() {
 
 
   return (
     <>
-    <div>weather app</div>
+    <div className='app'>
+      <div className='container'>
+        < Weather />
+    
+    <footer>
+      This projects was coded by <a href="#" targe="_blank">Lucy</a> and is {""} <a href="https://github.com/luccia1/weather-react target=-blank"> open-sourced on Github</a>
+    </footer>
+    </div>
+    </div>
     </>
   )
 }
