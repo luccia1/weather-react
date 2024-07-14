79445b89e8094f276da70108e46b9a20a5ceb18d
lucy <139963171+luccia1@users.noreply.github.com>
2024-07-14 14:19:38 -0700

    added the date component

--- my-react-app/src/Weather.jsx
+++ my-react-app/src/Weather.jsx
@@ -2,6 +2,7 @@
 import React, {useState} from "react";
 import "./Weather.css";
 import axios from 'axios';
+import FormattedDate from'./FormattedDate'
 
 export default function Weather(props){
 
@@ -12,7 +13,7 @@ function handleResponse(response){
         ready: true,
       temperature:  response.data.main.temp,
       humidity:response.data.main.humidity,
-      date: "Wednesday 07:00",
+      date:new Date(response.data.dt*1000),
       description:response.data.main.weather[0].description,
       iconUrl:"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII=",
       wind:response.data.wind.speed,
@@ -40,7 +41,9 @@ if(weatherData.ready){
            
            <h1>{weatherData.city}</h1> 
            <ul>
-            <li>{weatherData.date}</li>
+            <li>
+                <FormattedDate date={weatherData.date} />
+            </li>
             <li className="text-capitalize"> {weatherData.description}</li>
            </ul>
            <div className="row mt-3">
@@ -70,10 +73,10 @@ if(weatherData.ready){
     
 }
 else{
-    let apiKey="1266ad07b66517497b1acf79ea5a6a64";
+    let apiKey="3d19633teeafa6c79049ab3o334f7b44";
     let city="New York";
-    let apiUrl=`https://api.shecodes.io/weather/v1/current?
-    query=${props.defaultCity}$ appid=${apiKey} &units=metric`;
+    let apiUrl=`
+https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}$ appid=${apiKey} &units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
     
